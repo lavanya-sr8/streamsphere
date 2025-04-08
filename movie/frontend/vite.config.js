702e01aef28bb6ad94b9521557f18120b998c1e8
Rithika-27 <22z352@psgtech.ac.in>
2025-04-08 20:40:40 +0530

    hosting

--- movie/frontend/vite.config.js
+++ movie/frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://movie-frontend-9cf4.onrender.com/",
 			},
 		},
 	},
